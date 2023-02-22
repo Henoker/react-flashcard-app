ee2c9febe8b4e634f6e55c424e764663d68b2abd
Henoker <hennybany@gmail.com>
2023-02-22 13:26:40 +0300

    feat: flash card components styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
+import FlashcardList from "./components/FlashcardList";
+import './app.css'
 
 function App() {
+  const [flashcards, setflashcards] = useState(SAMPLE_FLASHCARDS)
   return (
-    <h1>Hello World</h1>
+    <FlashcardList flashcards={flashcards}/>
   );
 }
+
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: 'what is 2 + 2?',
+    answer: '4',
+    options: [
+      '2',
+      '3',
+      '4',
+      '5'
+    ]
+
+  },
+  {
+    id: 2,
+    question: 'what is 3 + 2?',
+    answer: '5',
+    options: [
+      '2',
+      '3',
+      '4',
+      '5'
+    ]
+
+  },
+  {
+    id: 3,
+    question: 'what is 1 + 2?',
+    answer: '3',
+    options: [
+      '2',
+      '3',
+      '4',
+      '5'
+    ]
+
+  }
+]
 
 export default App;
