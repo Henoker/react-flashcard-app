1cac28a7d23b02b55753915e2211b9bc772e3ed3
Henoker <hennybany@gmail.com>
2023-02-22 14:23:22 +0300

    feat: axios and api logic link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import FlashcardList from "./components/FlashcardList";
-import './app.css'
+import './app.css';
+import axios from "axios";
 
 function App() {
   const [flashcards, setflashcards] = useState(SAMPLE_FLASHCARDS)
+
+useEffect(() => {
+    axios
+      .get('https://opentdb.com/api.php?amount=10')
+      .then(res => {
+        setflashcards(res.data.results.map((questionItem, index) => {
+        const answer = decodeString(questionItem.correct_answer)
+        const options = [
+          ...questionItem.incorrect_answers.map(a => decodeString(a)), 
+          answer]
+            return {
+              id: `${index}-${Date.now()}`,
+              question: decodeString(questionItem.question),
+              answer: answer,
+              options: options.sort(() => Math.random() - .5)
+            }
+          })
+        )
+        console.log(res.data)
+      })
+  }, [])
+
+  function decodeString(str) {
+    const textArea = document.createElement('textarea')
+    textArea.innerHTML = str
+    return textArea.value
+  }
+
   return (
     <FlashcardList flashcards={flashcards}/>
   );
